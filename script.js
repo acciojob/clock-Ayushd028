2bac9ebd965c4405abb7cc682136c9f2521e56f6
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-13 20:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function updateTime() {
+            const now = new Date();
+            const formattedTime = now.toLocaleString();
+            document.getElementById("timer").textContent = formattedTime;
+        }
+        
+        setInterval(updateTime, 1000);
+        updateTime();
